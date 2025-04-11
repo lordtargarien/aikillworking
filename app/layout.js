36988ff2cc724d6dd930f2b2cb4043627234ce58
lordtargarien <2021godblessme@gmail.com>
2025-04-11 03:50:06 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,9 @@
 import "./globals.css";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0 }}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
